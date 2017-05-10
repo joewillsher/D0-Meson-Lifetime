dac017f529d00c53e3349a1e9c954dae421d863e
joewillsher <joewillsher@icloud.com>
2017-05-10 20:08:19 +0100

    Lifetime measurement
    This commit adds support for measuring the lifetime of a candidate
    event’s D0.
    
    Example output:
    <__main__.CandidateEvent object at 0x10e6be390>
    Dstar_OWNPV	[  0.5035  -0.0248 -24.7095]
    Dstar_ENDVERTEX	[ 1.40447044  0.50595707  0.41377701]
    D_ENDVERTEX	[ 1.1783  0.377  -5.7651]
    K		[   241.62    718.25  24313.59]
    Pd		[  3312.61   1329.99  73603.49]
    Ps		[  332.52936536   227.20692948  9303.84732173]
    
    Momentum components: [  3554.23   2048.24  97917.08]
    Displacement: 0.00618435963873
    Time:
    3.92533174909e-13s
    392.533174909 fs

--- Code/Lifetime.py
+++ Code/Lifetime.py
@@ -2,12 +2,18 @@ from typing import Text
 import csv
 import numpy as np
 from collections import namedtuple
+from scipy.constants import c, physical_constants
+e = physical_constants['electron volt'][0]
 
 # position 3-vector, units mm
 Position = np.array
 # momentum 3-vector, units MeV/c
 Momentum = np.array
 # Momentum = namedtuple('Momentum', ['x', 'y', 'z'])
+
+def magnitude(v: Position):
+	return np.sqrt(np.sum(v**2))
+
 
 # Keep all data about the type together in a type, so vals for an event are stored next to each other on the heap.
 class CandidateEvent(object):
@@ -29,6 +35,22 @@ class CandidateEvent(object):
 		str += "Pd\t\t" + np.array_str(self.pd) + "\n"
 		str += "Ps\t\t" + np.array_str(self.ps) + "\n"
 		return str
+	
+	def labFrameTravel(self):
+		return magnitude(self.dstarDecay-self.d0Decay) * 1e-3
+	
+	def absoluteDecayTime(self):
+		# p in a 3 axes is conserved in `D0 --> pi + K` decay
+		pcomps_d0 = self.kp+self.pd # in MeV
+		print(pcomps_d0)
+		p_d0 = magnitude(pcomps_d0) * 1e6 * e / c # in SI
+		m0_n = 1864.84 # MeV/c2
+		m0 = m0_n * 1e6 * e / c**2
+		# un-boosted time in the particle's frame
+		tp = m0 * self.labFrameTravel()  / p_d0
+		print(self.labFrameTravel(), tp)
+		return tp
+		
 		
 
 # reads a file and returns the D0 candidate events it lists
@@ -54,12 +76,18 @@ def readFile(name: Text):
         
         for row in rows:
         	# reshape row into several 3-vectors
-        	data = np.reshape(np.array(row), (len(row)/3, 3))
+        	nums = [float(x) for x in row]
+        	data = np.reshape(np.array(nums), (len(nums)/3, 3))
         	cand = CandidateEvent(data[elementIdxs[0]], data[elementIdxs[1]], data[elementIdxs[2]], data[elementIdxs[3]], data[elementIdxs[4]], data[elementIdxs[5]])
         	cands.append(cand)
         	
         return cands
         
-        
+
 data = readFile('np.txt')
-print(data)
+# print(data)
+
+d = data[0]
+print(d)
+time = d.absoluteDecayTime()
+print(str(time*1e15) + ' fs')
