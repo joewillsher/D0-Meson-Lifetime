cf1f6d73d1897f602b137206b6f9dafd60d53ef1
Lucille Calmon <ml.calmon@yahoo.com>
2017-06-05 17:26:53 +0100

    background subtraction and convoluted exponential with fixed parameters implemented in the maximum likelihood fit
    Done:
    -separates decay times in signal region/background sideband according
    to DeltaM.
    -calculates weight
    -takes into account the convolution with a gaussian (at fixed
    parameters) and applies weight factor when applicable in the maximum
    likelihood.
    -numerically computes the first and second derivatives of the negative
    log likelihood with respect to tau
    -finds the root of the derivative of the negative log likelihood with
    Newton raphson method
    -computes a statistical uncertainty on the root calculated above
    
    Why it might crash:
    -we need to obtain the negative log likelihood (after taking the
    product with respect to each t_i and the log) as a function of tau. If
    tau (an array) is substituted before, the product might not give the
    result expected.
    -consequence of said above, the differentiation with respect to tau
    might not work.
    -the neg log likelihood function is built awkwardly to try to tackle
    the problem of tau being an array and the compatibility of the product
    with respect to t_i.
    
    To do:
    -check that everything is working and gives expected results.
    -special care to the function Neg log likelihood
    -check the differentiation (and that it’s differentiating with the
    right variable)
    -print results (weight, lifetime, uncertainty) in special file
    -implement the lifetime in the fit
    -plot the decay distribution with the weight factor
    
    Lifetime.py
    -l. 318 in the definition of the convoluted exponential, make sure to
    add the factor of l/2 in the exponential exponent
    -end of file: change the weight calculation to wb = - (na/nb) and make
    sure na is right

--- Code/Fitting.py
+++ Code/Fitting.py
@@ -3,39 +3,73 @@ import csv
 import numpy as np
 from collections import namedtuple
 from scipy.constants import c, hbar, physical_constants
+from scipy.misc import derivative
 import pylab as pl
 import scipy.optimize as spo
 import lazy_property
 from Lifetime import *
 from Cuts import *
 
-times = [d.decayTime*1e12 for d in filtered if 10e-12 >= d.decayTime] #decay times considered
 
-ML = np.mean(times) #analytic mean lifetime
-print('Mean lifetime', ML)
+times = [d.decayTime*1e12 for d in filtered if 10e-12 >= d.decayTime] #decay times considered from filtered
+signal_region = [event for event in filtered if range_low <= event.d0Dstar_massdiff <= range_up] #events in the signal region
+times_sig = [d.decayTime*1e12 for d in signal_region] #decay times from events in the signal region
+background_region = [event for event in filtered if event not in signal_region] #events in the background sideband
+times_background = [d.decayTime*1e12 for d in background_region] #decay times from events in the sideband region
+
+wb = calculate_weight(after_po, filtered, after_bin_width)
+print (wb)
+
+weighted_decays = [i * wb for i in times_background] + times_sig #decay times with weight applied to events in the sideband region. Not used. Is it useful?
 
 #Negative log likelihood minimisation:
-N=len(times) #number of decays
-T=sum(times) #sum of all decay times
 range_tau = np.linspace(0.3, 0.7, 1000) #range of mean lifetime considered for minimisation
 
-def negative_log_likelihood(tau): #negative log likelihood as a function of tau
-    return N*np.log(tau) + T/tau
+def negative_log_likelihood(t, l): #t is decay time, l is lifetime
+    L_i = [] #creates empty list to append elements later
+    for x in t: #will compute the likelihood wrt tau for each event
+        if x in times_background: #applies weight to events in the sideband
+            L = (convoluted_exponential_1(x, 1, l, 0.5, 0)) ** wb #calculates the likelihood
+            L_i = L_i.append(L)
+        else :
+        L = convoluted_exponential_1(x, 1, l, 0.5, 0) #weight of 1 applied to events in the signal
+        L_i = L_i.append(L)
+    L = L_i.prod() #multiplies all the elements together WARNING: at this point, it still should be a function of tau
+    neg_log = - np.ln(L) #takes the negative of the log. NOW it should substitute tau
+    return neg_log
 
-def DL_Dtau(x): #first derivative wrt tau
-    return (N/x) - (T/(x**2))
+def D_Dtau(x_0): #first derivative wrt tau
+    DL_1 = []
+    for t in x_0: #x_0 is range of tau
+        DL = derivative(neg_log, t) #calculates the function at each point in range_tau
+        DL_1 = DL_1.append(DL)
+    return DL_1
 
-def DL_2_Dtau(x): #second derivative wrt tau
-    return -N/(x**2) + (2*T)/(x**3)
+def D_2_Dtau(x_0):
+    DL_2=[]
+    for t in x_0:
+        DL = derivative(neg_log, t, order=2)
+        DL_2 = DL_2.append(DL)
+    return DL_2
 
-#def convoluted_exponential(t, A, l, s, m):
-	#if s < 0 and m == 0:
-		#return 0
-	#return A* l/2 * np.exp(2*m + l * s**2 - 2*t) * sse.erfc((m + l * s**2 - t)/(2**0.5 * s))
+NL = negative_log_likelihood(times, range_tau)
+D_1 = D_Dtau(range_tau)
+D_2 = D_2_Dtau(range_tau)
+
+###plots to check the function above are working properly
+pl.plot(range_tau, NL)
+pl.savefig('L vs tau')
+pl.close()
+pl.plot(range_tau, D_1)
+pl.savefig('DL vs tau')
+pl.close()
+pl.plot(range_tau, D_2)
+pl.savefig('D2L vs tau')
+pl.close()
 
 def Newton_Raphson_tau(tau): #iterates and finds the root of the derivative (minimum of log likelihood)
-    while np.abs(DL_Dtau(tau)) >= 0.005: #precision to reach
-        tau_n = tau - DL_Dtau(tau)/DL_2_Dtau(tau)
+    while np.abs(D_Dtau(tau)) >= 0.005: #precision to reach
+        tau_n = tau - D_Dtau(tau)/D_2_Dtau(tau)
         tau = tau_n
     return tau #returns the root
 
@@ -43,15 +77,14 @@ tau_f = Newton_Raphson_tau(0.3)
 print ('tau_f', tau_f)
 
 #statistical uncertainty calculations
-
 def Neg_Log_1(tau): #Neg Log likelihood shifted by a threshold
-    return N*np.log(tau) + T/tau - negative_log_likelihood(tau_f) - 1
+    return negative_log_likelihood(times, tau) - negative_log_likelihood(times, tau_f) - 1 #WARNING: tau_f is a number, not an array, negative_log_likelihood might crash
 
 NL_1 = Neg_Log_1(range_tau)
 
 def Newton_Raphson_uncertainty(x): #finds the root of Neg_Log_1
     while np.abs(Neg_Log_1(x)) >= 0.005:
-        x_n = x - Neg_Log_1(x)/DL_Dtau(x)
+        x_n = x - Neg_Log_1(x)/D_Dtau(x)
         x=x_n
     return x
 
@@ -66,6 +99,3 @@ S = error(x_1, x_2, tau_f)
 
 print('lifetime ', tau_f, '+- ', S, ' ps')
 print('x1, x2', x_1, x_2)
-
-#wb = calculate_weight(after_po, filtered, after_bin_width)
-#print (wb)
