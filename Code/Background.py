Fix bug in time calculation; correct values now

Code/Background.py
@@ -8,9 +8,8 @@ from scipy.constants import c, hbar, physical_constants
 e = physical_constants['electron volt'][0]
 m_pi, m_k = 139.57018, 493.677 # TODO: uncert 0.00035, 0.013 respectively
 
-def convoluted_exponential(t, A, tau, s, m):
-	l = 1/tau
-	return A* l/2 * np.exp(l/2 * (2*m + l * s**2 - 2*t)) * sse.erfc((m + l * s**2 - t)/(2**0.5 * s))
+def convoluted_exponential(t, A, tau, s):
+	return A * 1/(2*tau) * np.exp(1/tau * (s**2/(2*tau) - t)) * (1- sse.erf((s**2 / tau - t)/(2**0.5 * s)))
 
 def gaussian(t, A, s, m):
 	return A * np.exp(-(t-m)**2/(2*s**2))
@@ -20,6 +19,9 @@ def double_gaussian(t, A, m, s1, s2, f):
 		(1-f) * np.exp(-(t-m)**2/(2*s2**2)))
 
 
+def normalisation_const(pdf, range, args):
+	i = spi.quad(pdf, range[0], range[1], args=args)[0]
+	return 1/i
 
 def background_fit(dm, bg_A, bg_p, bg_m):
 	return bg_A * (dm-bg_m)**bg_p

Code/Fitting.py
@@ -20,29 +20,25 @@ def maximum_likelyhood_exp_fit(full_set, after_po, deltamass_peak_width):
 	range_low, range_up = get_sig_range(after_po, deltamass_peak_width)
 	print(range_low, range_up)
 	
-	data = [event for event in full_set if 0 <= event.decayTime <= 10e-12]
+	fit_range = (0, 10)
+	pdf_gaussian_width = 1./10.
+	
+	data = [event for event in full_set if fit_range[0] <= event.decayTime*1e-12 <= fit_range[1]]
 
 	wb = calculate_weight(after_po, data, range_low, range_up)
 		
 	times = [d.decayTime*1e12 for d in data] #decay times considered from data
 	mass_diffs = [event.massDiff_d0dstar for event in data] #decay times considered from data
 	
-	signal_region = [event for event in data if range_low <= event.massDiff_d0dstar <= range_up] #events in the signal region
-	times_sig = [d.decayTime*1e12 for d in signal_region] #decay times from events in the signal region
-	background_region = [event for event in data if event not in signal_region] #events in the background sideband
-	times_background = [d.decayTime*1e12 for d in background_region] #decay times from events in the sideband region
 	
-	N = len(data)
-	
-	pdf_gaussian_width = .88945173193
-	
-	def pdf(ti, l):
-		return convoluted_exponential(ti, 1, l, pdf_gaussian_width, 0)
+	def pdf(ti, tau, A):
+		return convoluted_exponential(ti, A, tau, pdf_gaussian_width)
 
-	def negative_log_likelihood(l, ts, mdiffs): #ts, mdiffs are the events' times and mass diffs, l is lifetime
-		aaa = [- (1 if range_low <= md <= range_up else wb) * np.log(pdf(x, l)) for x, md in zip(ts, mass_diffs)]
-		print(sum(aaa), l)
-		return sum(aaa)
+	def negative_log_likelihood(tau, ts, mdiffs): #ts, mdiffs are the events' times and mass diffs, l is lifetime
+		normalisation = normalisation_const(convoluted_exponential, fit_range, (1, tau, pdf_gaussian_width))
+		aaa = [(1 if range_low <= md <= range_up else wb) * np.log(pdf(x, tau, normalisation)) for x, md in zip(ts, mass_diffs)]
+		print(-sum(aaa), tau)
+		return -sum(aaa)
 
 	def D_Dtau(tau_x, ts, mdiffs): #first derivative wrt tau
 		return derivative(negative_log_likelihood, tau_x, args=(times, mdiffs), dx=1e-5)
@@ -66,8 +62,9 @@ def maximum_likelyhood_exp_fit(full_set, after_po, deltamass_peak_width):
 	print('tau', tau_f, np.mean(times))
 	
 	newfig()
-	x = np.linspace(0.2, 0.6, 100)
-	pl.plot(x, negative_log_likelihood(x, times, mass_diffs))
+	x = np.linspace(.25, .65, 100)
+	A = normalisation_const(convoluted_exponential, fit_range, (1, tau_f, pdf_gaussian_width))
+	pl.plot(x, [negative_log_likelihood(x, times, mass_diffs) for x in x])
 	savefig('L vs tau')
 	
 	
@@ -83,10 +80,10 @@ def maximum_likelyhood_exp_fit(full_set, after_po, deltamass_peak_width):
 			x=x_n
 		return x
 	
-	x_1 = Newton_Raphson_uncertainty(tau_f - 0.02)
-	x_2 = Newton_Raphson_uncertainty(tau_f + 0.02)
+	x_1 = Newton_Raphson_uncertainty(tau_f - 0.01)
+	x_2 = Newton_Raphson_uncertainty(tau_f + 0.01)
 	S = np.abs(x_1 - x_2)/2
 	
 	print('lifetime ', tau_f, '+- ', S, ' ps')
-	return tau_f, S, wb
+	return tau_f, S, wb, pdf_gaussian_width, A
 

Code/Lifetime.py
@@ -212,7 +212,7 @@ class CandidateEvent(object):
 
 	@lazy_property.LazyProperty
 	def decayTime(self):
-		x = (self.d0Decay-self.dstarDecay)*1e-3
+		x = (self.d0Decay-self.bDecay)*1e-3
 		m_d0 = self.reconstructedD0Mass
 		p_d0 = self.pD0_comps
 		return np.sign(dot(x, p_d0)) * magnitude(x) * m_d0 / magnitude(p_d0)
@@ -263,7 +263,7 @@ class CandidateEvent(object):
 
 	@lazy_property.LazyProperty
 	def costheta(self):
-		p, r = self.kp+self.pd+self.ps, self.dstarDecay-self.interaction
+		p, r = self.kp+self.pd, self.d0Decay-self.dstarDecay
 		return dot(p/magnitude(p), r/magnitude(r))
 		
 		
@@ -347,43 +347,54 @@ def plotData(data):
 def calculateLifetime(data, bg, deltamass_po, deltamass_peak_width):
 	bg_integral, sig_integral, bg_fraction = estimate_background(deltamass_po, data, deltamass_peak_width)
 	
-	tau_elimination, tau_elimination_err, wb = maximum_likelyhood_exp_fit(data, deltamass_po, deltamass_peak_width)
+	tau_elimination, tau_elimination_err, wb, pdf_gaussian_width, A = \
+		maximum_likelyhood_exp_fit(data, deltamass_po, deltamass_peak_width)
 	
 	filtered = [d for d in data if -4e-12 <= d.decayTime < 10e-12]
 
-	range_low, range_up = 142.414041989, 149.059198523
+	range_low, range_up = get_sig_range(deltamass_po, deltamass_peak_width)
 
 	times = [d.decayTime*1e12 for d in filtered]
 	np.save('TIMES', times)
 	weights = [(1 if range_low <= d.massDiff_d0dstar <= range_up else wb) for d in filtered]
+	bg_weights = [(0 if range_low <= d.massDiff_d0dstar <= range_up else 1) for d in filtered]
 
-	time_range, bin_num = (-4, 10), 120
+	time_range, bin_num = (-.4, 10), 50 if is_latex else 300
 
 	# decay time curve
 	hist, bin_edges = np.histogram(times, bins=bin_num, range=time_range, weights=weights)	
-	hist_bg = np.histogram(times, bins=bin_num, range=time_range, normed=True)[0] * np.sum(hist) * bg_fraction
+	hist_bg, _ = np.histogram(times, bins=bin_num, range=time_range, weights=bg_weights)
 	hist_raw, _ = np.histogram(times, bins=bin_num, range=time_range)	
 
 	sy = np.histogram(times, bins=bin_edges, weights=times)[0]
 	time = bin_edges[1:]
 	errors = [x*.9999999999 if x <= 1 else np.sqrt(x) for x in hist-.0000000001]
 
-	newfig()
+# 	po, po_cov = spo.curve_fit(lambda t, a, tau: a * np.exp(-t/tau), time, hist, [3000, .4], sigma=errors, absolute_sigma=True)
+# 	po_conv, po_cov_conv = spo.curve_fit(convoluted_exponential, time, hist, [3000, .41, .5], errors, absolute_sigma=True)
+# 	print(po, po_conv)
+
+	time_cont = np.linspace(time_range[0], time_range[1], 1000)
+	
+	fig, ax = newfig()
 	pl.semilogy(time, hist, '.g')
 	pl.semilogy(time, hist_bg, '.k')
 	pl.semilogy(time, hist_raw, '.r')
 	pl.errorbar(time, hist, yerr=errors, fmt=',r', capsize=0)
-	pl.semilogy(time, convoluted_exponential(time, 1e4, tau_elimination, .88945173193, 0))
+	pl.semilogy(time_cont, convoluted_exponential(time_cont, max(hist)*.7, tau_elimination, pdf_gaussian_width), '-g')
 	pl.xlabel(r'Decay time [ps]')
+# 	ax.set_ylim(1e-2, 1e4)
+	ax.set_xlim(time_range)
 	savefig('decay-fitted')
 	pl.close()
 
-	newfig()
+	fig, ax = newfig()
 	pl.plot(time, hist, '.g')
 	pl.plot(time, hist_bg, '.k')
 	pl.plot(time, hist_raw, '.r')
 	pl.errorbar(time, hist, yerr=errors, fmt=',r', capsize=0)
-	pl.plot(time, convoluted_exponential(time, 6000, tau_elimination, .88945173193, 0))
+	pl.plot(time_cont, convoluted_exponential(time_cont, max(hist)*.7, tau_elimination, pdf_gaussian_width), '-g')
+	ax.set_xlim(time_range[0], 4)
 	pl.xlabel(r'Decay time [ps]')
 	savefig('decay')
 	pl.close()